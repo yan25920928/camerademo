9931726b3ccdc75b9d85e2b6697cef339a6120a7
yan <645786233@qq.com>
2018-10-11 19:27:54 +0800

    使用Camera2API重新开放自定义相机

---
+++ app/src/main/java/com/example/yan/camerademo/ImageSaver.java
@@ -0,0 +1,62 @@
+package com.example.yan.camerademo;
+
+import android.app.Application;
+import android.media.Image;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * 获取Image的帧数据，输出到指定的文件里，文件名用当前时间来生成
+ * Application也可以不用外部传入，另一种方式可以提取一个公共方法getApplication()获取
+ */
+public class ImageSaver implements Runnable{
+
+   private Image mImage;
+   private File mFile;
+   private Application mApplication;
+   public ImageSaver(Image image, Application application){
+       mImage = image;
+       mApplication = application;
+   }
+
+    /**
+     * IO流，保存Image数据
+     */
+    @Override
+    public void run() {
+        ByteBuffer buffer = mImage.getPlanes()[0].getBuffer();
+        byte[] bytes = new byte[buffer.remaining()];
+        buffer.get(bytes);
+        FileOutputStream outputStream = null;
+
+        //设置时间
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.US);
+
+        //设置名称
+        String fname = "IMG"+sdf.format(new Date())+".jpg";
+        mFile = new File(mApplication.getExternalFilesDir(null),fname);
+        //写入操作
+        try {
+            outputStream = new FileOutputStream(mFile);
+            outputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            //关闭资源
+            mImage.close();
+            if (null != outputStream){
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/yan/camerademo/MainActivity.java
+++ app/src/main/java/com/example/yan/camerademo/MainActivity.java
@@ -1,66 +1,256 @@
 package com.example.yan.camerademo;
 
+
+import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.provider.MediaStore;
+import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+
+import android.view.TextureView;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    private Handler mHandler = new Handler();
+    private TextureView mTextureView;
+    private String mCameraId;
+    private CameraDevice mCameraDevice;
+    private Size mPreviewSize;
+    private ImageReader mImageReader;
+    private CaptureRequest.Builder mPreviewBuilder;
+    private CameraCaptureSession mPreviewSerssion;
+    private CaptureRequest mCaptureRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkCameraHardware(this);
+        initView();
+        initListener();
     }
 
-    private boolean checkCameraHardware(Context context){
-        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            Log.d("MainActivity","有摄像头");
-            getNumOfCameras();
-            getCameraInstance();
-            return true;
-        }else {
-            Log.d("MainActivity","无摄像头");
-            return false;
-        }
+    private void initView() {
+        mTextureView = findViewById(R.id.textureView);
     }
 
-    private int getNumOfCameras(){
-        Log.d("MainActivity","摄像头:"+Camera.getNumberOfCameras());
-        return Camera.getNumberOfCameras();
+    private void initListener() {
+        /**
+         * 设置TextureView的回调监听
+         */
+        mTextureView.setSurfaceTextureListener(new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                setupCamera();
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        });
     }
 
-    public static Camera getCameraInstance(){
-        Camera camera = null;
+    /**
+     * 设置默认启用后置摄像头，并且设置图像尺寸获取摄像头的ID
+     * 通过CameraManager，遍历所有摄像头
+     * 根据摄像头特征，默认启用后置
+     */
+    private void setupCamera() {
+        //获取CameraManager
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
-            camera = Camera.open(1);
-        }catch (Exception e){
-            Log.d("MainActivity","EXCEPTION:");
+            //遍历所有摄像头
+            for (String id : cameraManager.getCameraIdList()) {
+                //获取相机特征
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(id);
+                //LENS_FACING_FRONT——前置，LENS_FACING_BACK——后置，LENS_FACING_EXTERNAL——外置
+                if (characteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT)
+                    //非后置时，直接跳出循环
+                    continue;
+                //获取StreamConfigurationMap，它是管理摄像头支持的所有输出格式和尺寸
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                // 设置预览静态图片，使用最大尺寸
+                mPreviewSize = Collections.max(Arrays.asList(map.getOutputSizes(ImageFormat.JPEG)),
+                        new Comparator<Size>() {
+                            @Override
+                            public int compare(Size o1, Size o2) {
+                                return Long.signum(o1.getWidth() * o1.getHeight() - o2.getWidth() * o2.getHeight());
+                            }
+                        });
+                mCameraId = id;
+                break;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
-        return camera;
     }
 
-    private boolean safeCameraOpen(int id){
-        boolean qOpened = false;
+    /**
+     * 打开相机
+     */
+    private void openCamera() {
+        //获取CameraManager
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        //检查权限
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
 
-        return qOpened;
+        //  打开相机
+        //  第一个参数指示打开哪个摄像头
+        //  第二个参数stateCallback为相机的状态回调接口
+        //  第三个参数用来确定Callback在哪个线程执行，为null的话就在当前线程执行
+        try {
+            cameraManager.openCamera(mCameraId, stateCallback, mHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void releaseCameraAndPreview(){
+    /**
+     * 启用我们设备的回调开始预览
+     */
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            //初始化CameraDevice
+            mCameraDevice = camera;
+            //开启预览
+            startPreview();
+        }
 
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+
+        }
+    };
+
+    /**
+     * 开启预览界面:设置Surface把它与CaptureRequestBuilder对象关联，再设置会话开始捕获画面
+     */
+    private void startPreview() {
+        SurfaceTexture mSurfaceTexture = mTextureView.getSurfaceTexture();
+        //设置缓冲区大小
+        mSurfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        //获取surface显示预览数据
+        Surface mSurface = new Surface(mSurfaceTexture);
+        //读取图片
+        setupImageReader();
+        //获取ImageReader的surface
+        Surface imageReaderSurface = mImageReader.getSurface();
+        try {
+            //创建CaptureRequest.Builder对象，TEMPLATE_PREVIEW表示预览请求
+            mPreviewBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            //设置surface作为预览数据的显示界面
+            mPreviewBuilder.addTarget(mSurface);
+            //创建相机捕获会话，
+            // 第一个参数是捕获数据的输出Surface列表，
+            // 第二个参数是CameraCaptureSession的状态回调接口，当它创建好后会回调onConfigured方法，
+            // 第三个参数用来确定Callback在哪个线程执行，为null的话就在当前线程执行
+            mCameraDevice.createCaptureSession(Arrays.asList(mSurface, imageReaderSurface), mSessionStateCallback, mHandler);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void dispatchTakePictureIntent(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager())!=null){
-            startActivityForResult(takePictureIntent,REQUEST_IMAGE_CAPTURE);
+    /**
+     * 读取图片
+     */
+    private void setupImageReader() {
+        //指定尺寸宽，高 和 图片格式，以及最多获取图像流的帧数
+        mImageReader = ImageReader.newInstance(mPreviewSize.getWidth(), mPreviewSize.getHeight(),
+                ImageFormat.JPEG, 2);
+
+        mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader reader) {
+                mHandler.post(new ImageSaver(reader.acquireNextImage(), getApplication()));
+            }
+        }, mHandler);
+    }
+
+    /**
+     * CameraCaptureSession状态接口回调
+     */
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(@NonNull CameraCaptureSession session) {
+            //创建捕获请求
+            mCaptureRequest = mPreviewBuilder.build();
+            mPreviewSerssion = session;
+            //设置反复捕获数据的请求，这样预览界面就会一直有数据传送显示
+            try {
+                mPreviewSerssion.setRepeatingRequest(mCaptureRequest, mSessionCaptureCallback, null);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+        }
+    };
+
+    /**
+     * CameraCaptureSession.CaptureCallback 设置预览完成的逻辑处理
+     */
+    private CameraCaptureSession.CaptureCallback mSessionCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureCompleted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, @NonNull TotalCaptureResult result) {
+            super.onCaptureCompleted(session, request, result);
+            //重启预览
+            restartPreview();
+        }
+    };
+
+    /**
+     * 重启预览
+     */
+    private void restartPreview() {
+        try {
+            //执行setRepeatingRequest方法就行了，注意mCaptureRequest是之前开启预览设置的请求
+            mPreviewSerssion.setRepeatingRequest(mCaptureRequest, null, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
         }
     }
 }
